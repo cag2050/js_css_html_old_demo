aeea535c6e42e272dda1d8bee8a636f02d4e959a
chenanguo <chenanguo@le.com>
2016-10-21 14:55:59 +0800

    m

---
+++ src/js/es6/let.js
@@ -0,0 +1,4 @@
+/**
+ * Created by chenanguo on 2016/10/21.
+ */
+let aannn = {};
---
+++ webpack.config.js
@@ -0,0 +1,49 @@
+/**
+ * Created by chenanguo on 2016/9/29.
+ */
+var path = require("path"),
+    fs = require("fs");
+jsEs6 = "/js/es6/";
+
+// 获得es6文件夹下的js文件
+var getJsFiles = function () {
+    var jsPath = path.resolve("src") + jsEs6;
+    // console.log(jsPath);
+    var dirs = fs.readdirSync(jsPath);
+    var matchs = [],
+        files = {},
+        all = [];
+    dirs.forEach(function (item) {
+        matchs = item.match(/(.+)\.js$/);
+        var _path = '';
+        if (matchs) {
+            _path = path.resolve("src" + jsEs6, item);
+            files[matchs[1]] = _path;
+            all.push(_path);
+        }
+    });
+    return files;
+};
+
+var config = {
+    entry: getJsFiles(),
+    output: {
+        // path: "build",
+        path: path.join(__dirname, "build" + jsEs6),
+        filename: "[name].js"
+    },
+    module: {
+        loaders: [
+            {
+                test: /\.js$/,
+                exclude: "/node-modules/",
+                loader: "babel",
+                query: {
+                    presets: ["es2015"]
+                }
+            }
+        ]
+    }
+};
+// console.log(config);
+module.exports = config;
